cbb46b372f1944b92ea8f99040421e61def58ebd
andrezaaraujo <andreza.si.araujo@gmail.com>
2022-09-14 21:48:17 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv-safe").config();
+
+const db = require("./database/mongoConfig");
+db.connect();
+
+app.use(cors());
+app.use(express.json());
+const usersRoutes = require("./routes/usersRoutes");
+
+ app.use("/users", usersRoutes);
+
+module.exports = app;
